3f65d4f1e3eefb561623bcd9c806457ab8fa6f6e
DanXMZ <114554015+DanXMZ@users.noreply.github.com>
2022-10-05 00:08:07 -0500

    Add files via upload

---
+++ strings_Julia.jl
@@ -0,0 +1,46 @@
+#Operaciones con cadenas de caracteres(Strings)
+
+var1="abcdef"
+
+#Concatenar dos cadenas de caracteres(* operator)
+
+"Julia "*"Language"
+
+#otra forma
+
+Greeting="What's up?";
+Name="DanX"
+
+Greeting*","*Name
+
+#Repetir cadena (^ operator)
+
+"Julia language."^4
+
+Greeting^3
+
+x,y,z="let's ","to ","repeat."
+
+x*y*z
+
+(x*y*z)^2
+
+x*y*z^3
+
+#caracteres especiales imprimibles que no estan en teclado, por ejemplo alfabeto griego
+#usamos "\ + nombre_caracter(letra)+TAB(teclado) ej. \alpha[TAB]
+
+println("Imprimiré las tres primeras letras del alfabeto griego:")
+println("α")
+println("β")
+println("γ")
+
+#Como nombre de variables
+
+δ="Esto es una cadena de caracteres."
+
+δ^2
+
+'a'+1
+
+
